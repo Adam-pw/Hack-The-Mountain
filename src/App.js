c97115191e77a7d7a0320409eb1a4ffa70e9e02b
Adam-pw <adampithewan@gmail.com>
2022-09-12 06:17:45 -0400

    Initialized project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Cards } from "./Compenents/Cards/Cards";
+import Carouselhome from "./Compenents/Carousel/Carousel";
+import Navbarhome from "./Compenents/Navbar/Navbar.jsx";
+import { MantineProvider } from "@mantine/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Navbarhome />
+        <Carouselhome />
+        {/* <Cards /> */}
+      </MantineProvider>
+    </>
   );
 }
 
